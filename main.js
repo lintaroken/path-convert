11ceef9559161c2a932f9fd3bdc5528410496c45
lintaroken <k@8-3.cc>
2018-12-27 23:31:30 +0900

    コピペに対応した

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const { ipcMain, ipcRenderer } = require('electron');
+const Menu = electron.Menu;
 
 let mainWindow = null;
 app.on('ready', () => {
@@ -14,5 +14,25 @@ app.on('ready', () => {
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+
+  const template = [{
+    label: "Application",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+  ];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
 
